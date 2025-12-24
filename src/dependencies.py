e68ff2904e44e12c88844fab9eb6e0e586c988ab
bransoned <bransonmb@proton.me>
2025-12-24 01:49:54 +0000

    #25 combine logic for finding exiftool/ffmpeg

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,22 +1,22 @@
-import sys
+from .exceptions import *
 from pathlib import Path
 import shutil
-from .exceptions import *
+import sys
 
 # =========================================================================== #
 
 """
-Find exiftool executable
+Find dependency executable
 
 Returns:
-    Path to exiftool
+    Path to dependency
 
 Raises:
-    DependencyError: If exiftool cannot be found
+    DependencyError: If dependency cannot be found
 """
-def find_exiftool() -> str:
+def find_dependency(dependency_str: str) -> str:
 
-    exe_name = "exiftool.exe" if sys.platform.startswith("win") else "exiftool"
+    exe_name = f"{dependency_str}.exe" if sys.platform.startswith("win") else f"{dependency_str}"
 
     # If bundle
     if getattr(sys, "frozen", False):
@@ -31,40 +31,9 @@ def find_exiftool() -> str:
         return system_path
 
     raise DependencyError(
-        "Exiftool not found. Please install exiftool or use the provided bundled executable."
+        f"{depency_str} not found. Please install {dependency_str} or use the provided bundled executable."
         "For further installation instructions, reference the README."
     )
 
 # =========================================================================== #
 
-"""
-Find ffmpeg executable.
-
-Returns:
-    Path to ffmpeg
-
-Raises:
-    DependencyError: If ffmpeg cannot be found
-"""
-def find_ffmpeg() -> str:
-
-    exe_name = "ffmpeg.exe" if sys.platform.startswith("win") else "ffmpeg"
-
-    # If bundle
-    if getattr(sys, "frozen", False):
-        base = Path(sys._MEIPASS)
-        bundled = base / "bin" / exe_name
-        if bundled.exists():
-            return str(bundled)
-
-    # Try system PATH
-    system_path = shutil.which(exe_name)
-    if system_path:
-        return system_path
-
-    raise DependencyError(
-        "FFMpeg not found. Please install ffmpeg or use the provided bundled executable."
-        "For further installation instructions, reference the README."
-    )
-
-# =========================================================================== #
