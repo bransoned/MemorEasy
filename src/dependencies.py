79383ff1b7459bef57f654c478f9e287a7ab9270
bransoned <bransonmb@proton.me>
2025-12-24 05:44:17 +0000

    #37 fix linting errors for dependencies.py

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,4 +1,4 @@
-from .exceptions import *
+from .exceptions import DependencyError
 from pathlib import Path
 import shutil
 import sys
@@ -14,9 +14,14 @@ Returns:
 Raises:
     DependencyError: If dependency cannot be found
 """
+
+
 def find_dependency(dependency_str: str) -> str:
 
-    exe_name = f"{dependency_str}.exe" if sys.platform.startswith("win") else f"{dependency_str}"
+    if sys.platform.startswith("win"):
+        exe_name = f"{dependency_str}.exe"
+    else:
+        exe_name = f"{dependency_str}"
 
     # If bundle
     if getattr(sys, "frozen", False):
@@ -31,9 +36,9 @@ def find_dependency(dependency_str: str) -> str:
         return system_path
 
     raise DependencyError(
-        f"{depency_str} not found. Please install {dependency_str} or use the provided bundled executable."
+        f"{dependency_str} not found. Please install {dependency_str} or"
+        "use the provided bundled executable."
         "For further installation instructions, reference the README."
     )
 
 # =========================================================================== #
-
